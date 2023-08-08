2797e8d4423abd03a8769aacf5300f132d6616c9
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-08-08 05:05:20 +0100

    wrote basic functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import './App.css'
+import React, { useState } from 'react';
+import "./App.css"
 
 function App() {
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedColor(event.target.value);
+  };
+
+  const copyColor = () => {
+    navigator.clipboard.writeText(selectedColor);
+  }
 
   return (
-    <>
-      <h2>Hello world</h2>
-    </>
-  )
+    <div>
+      <input
+        type="color"
+        value={selectedColor}
+        onChange={handleColorChange}
+      />
+      <div>
+        <button onClick={copyColor}>copy</button>
+        <p>Selected color: {selectedColor}</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
