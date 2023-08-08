2fb6675f2e9a1749f3df84b2a3564fe79b801b83
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-08-08 20:30:59 +0100

    added two way binding

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
-import React, { useState } from 'react';
-import "./App.css"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const [selectedColor, setSelectedColor] = useState('');
+  const [selectedColor, setSelectedColor] = useState("");
+  const [defaultColor, setDefaultColor] = useState("");
+  const [show, setShow] = useState(false);
 
   const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedColor(event.target.value);
+    setDefaultColor(event.target.value);
   };
 
-  const copyColor = () => {
-    navigator.clipboard.writeText(selectedColor);
-  }
+  useEffect(() => {
+    setSelectedColor(defaultColor);
 
+    if (defaultColor.length === 7 && defaultColor[0] === "#") {
+      setShow(true);
+    }
+  }, [defaultColor]);
+
+  // useEffect(() => {
+  //   if (selectedColor.length === 7 && selectedColor[0] === "#") {
+  //     setDefaultColor(selectedColor);
+  //   }
+  // }, [selectedColor]);
+      
   return (
-    <div>
-      <input
-        type="color"
-        value={selectedColor}
-        onChange={handleColorChange}
-      />
-      <div>
-        <button onClick={copyColor}>copy</button>
-        <p>Selected color: {selectedColor}</p>
+    <div className="py-4 w-[300px] px-4">
+      <div className="flex items-center">
+        <input
+          type="color"
+          value={defaultColor}
+          onChange={handleColorChange}
+          className="cursor-pointer w-[30%]"
+        />
+        <input
+          type="text"
+          placeholder="#000000"
+          value={defaultColor}
+          className="w-[70%] ml-2 border-2 border-[#ccc] outline-none px-3 py-0.5 rounded-md placeholder:text-[14px] text-[14px]"
+          onChange={(e) => setDefaultColor(e.target.value)}
+        />
       </div>
+      {show && (
+        <div className="flex justify-between pt-2 items-center">
+          <p>
+            Selected color: <span className="font-bold">{selectedColor}</span>
+          </p>
+          {/* <button onClick={copyColor}>copy</button> */}
+          <Dropdown selectedColor={selectedColor} />
+        </div>
+      )}
     </div>
   );
 }
